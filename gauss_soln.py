f43e7371de3732ea1c31492c22337d9b81739cc5
Aleksandar Diamond-Stanic <adiamond@a23379.bates.edu>
2016-09-11 11:09:04 -0400

    added two-panel plot to gauss_soln.py

--- gauss_soln.py
+++ gauss_soln.py
@@ -18,11 +18,13 @@ x = np.arange(nel) * dx + min
 # define the profile from the book
 f = 3 / (10 * x**2 + 1)
 
-#plt.plot(x, f)
-
 # define the gaussian function
 g = gaussian(x, 3.0, 0., 0.3)
-#plt.plot(x,g)
+
+# plot the profiles
+ax = plt.subplot(1,2,1)
+ax.plot(x, f, color='blue')
+ax.plot(x, g, color='red')
 
 # integrate f and g over the full range of x
 f_int = np.sum(f)
@@ -30,8 +32,7 @@ print("function area", f_int)
 g_int = np.sum(g)
 print("gaussian area", g_int)
 
-# consider the fraction of the total area over a given width and
-# create placeholder arrays to store this information
+# consider the fraction of the total area over a given width
 nel = max / dx + 1
 width = np.arange(nel) * dx
 f_frac = np.zeros(nel)
@@ -42,7 +43,6 @@ for i in np.arange(nel):
     rel = np.where(np.abs(x) < width[i])
     f_frac[i] = np.sum(f[rel]) / f_int
     g_frac[i] = np.sum(g[rel]) / g_int
-    #print(width[i], f_frac[i], g_frac[i])
 
 # what width encloses 68% of the total area?
 f_68 = f_frac > 0.68
@@ -56,8 +56,10 @@ print("function 95% width",width[f_95][0], f_frac[f_95][0])
 g_95 = g_frac > 0.95
 print("gaussian 95% width",width[g_95][0], g_frac[g_95][0])
 
-plt.plot(width, f_frac)
-plt.plot(width, g_frac)
+# plot the percentage vs width
+ax = plt.subplot(1,2,2)
+ax.plot(width, f_frac, color='blue')
+ax.plot(width, g_frac, color='red')
 
 plt.show()
 
